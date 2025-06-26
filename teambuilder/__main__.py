c3f5b55e1f555c05a83cd8af094d358c130426bc
mildlydeterministic <briano.planeta@gmail.com>
2025-06-26 21:54:36 +0000

    Fix sys.path setup to include parent directory in entrypoint

--- teambuilder/__main__.py
+++ teambuilder/__main__.py
@@ -6,7 +6,7 @@ See /docs/spec.md for full specification.
 """
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from teambuilder.cli import main
 
 if __name__ == "__main__":
